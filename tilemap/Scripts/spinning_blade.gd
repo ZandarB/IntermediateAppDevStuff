e4789eaf229f72084e827bdd675a43061282040a
Zandar Beker (1000126240) <BEKEZA1@student.op.ac.nz>
2025-09-03 12:52:24 +1200

    Build: replaced spikes and spinning blades from tilemap with their own scene instances with new sprites etc, got touching detection working with them

---
+++ tilemap/Scripts/spinning_blade.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var damage = 10
+var ready_to_detect = false
+
+func _ready():
+	call_deferred("_enable_detection")
+
+func _enable_detection():
+	ready_to_detect = true
+
+func _on_body_entered(body):
+	print("Player touched the blade!")
